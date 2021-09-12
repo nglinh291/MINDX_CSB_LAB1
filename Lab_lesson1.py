4e4a1c26dfdf6c86405ecd50ca65f88ac223d3fe
nglinh291 <cakiela291@gmail.com>
2021-09-12 23:31:09 +0700

    MINDX_CSB_Lab1

---
+++ Lab_lesson1.py
@@ -0,0 +1,91 @@
+
+# Bài 1
+name = input('Your name')
+print("Welcome", name)
+
+# Bài 2
+first_name = input('First name ')
+last_name = input('Last name ')
+phone_number = input('Phone number ')
+print("Your registered name is", first_name, last_name, ' Your phone number is', phone_number)
+
+# Bài 3
+print("Name: Ngọc Linh\nBirthplace: 29/01/2001")
+
+# Bài 4
+year = input('What year is it?')
+print("                               .  :  .           ")
+print("                             '.   :   .'       ")
+print("HAPPY NEW YEAR              .   '.:.'   .       ")
+print("!!!", year, "!!!","         .   .':'.   .       " )
+print("                             '.   :   .'       ")
+print("                               '  :  '           ")
+
+# Bài 5
+from turtle import *
+forward(100)
+left(120)
+forward(100)
+left(120)
+forward(100)
+left(120)
+forward(50)
+left(90)
+
+# Bài 6
+from turtle import *
+pensize(5)
+forward(200)
+left(90)
+forward(200)
+left(90)
+forward(200)
+left(90)
+forward(200)
+left(90)
+penup()
+forward(30)
+left(90)
+forward(30)
+pendown()
+pensize(2)
+forward(140)
+right(90)
+forward(140)
+right(90)
+forward(140)
+right(90)
+forward(140)
+right(90)
+penup()
+forward(70)
+right(90)
+forward(70)
+left(90)
+
+# Bài 7
+from turtle import *
+forward(100)
+left(90)
+forward(100)
+left(90)
+forward(100)
+left(90)
+forward(100)
+left(90)
+penup()
+forward(50)
+right(90)
+forward(20)
+left(135)
+pendown()
+forward(100)
+left(90)
+forward(100)
+left(90)
+forward(100)
+left(90)
+forward(100)
+penup()
+left(135)
+forward(70)
